d7b3d3dd5bf6b92473fbd0e4d11cf5e898c10c31
EC2 Default User <ec2-user@ip-172-31-38-207.ec2.internal>
2022-01-31 18:52:02 +0000

    Lab2

---
+++ Lab2.py
@@ -0,0 +1,31 @@
+#3.1
+my_name = "Tom"
+print(my_name)
+
+#3.2
+my_id = 123
+print(my_id)
+
+#3.3
+#_123 = my_id
+#Variables cannot start with numbers
+my_id = your_id = 123
+print(my_id)
+print(your_id)
+
+#3.4
+my_id_str = '123'
+print(my_id_str)
+
+#3.5
+#print(my_name+my_id)
+#Cannot add a string to an integer
+
+#3.6
+print(my_name+my_id_str)
+
+#3.7
+print(my_name*3)
+
+#3.8
+print('Hello world. This is my first python string.'.split('.'))
