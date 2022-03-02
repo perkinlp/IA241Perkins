5e11d4bafc423c4587a911d4d6fa6999d4822fab
EC2 Default User <ec2-user@ip-172-31-38-207.ec2.internal>
2022-03-02 04:16:14 +0000

    Lab5

---
+++ Lab5.py
@@ -0,0 +1,39 @@
+#5.1
+alien_color = 'green'
+if alien_color == 'green':
+    print('You just earned 5 points')
+
+#5.2
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('You just earned 5 points')
+
+else:
+    print('You just earned 10 points')
+
+#5.3
+favorite_fruits = ['strawberry','blueberry','honeydew']
+
+if 'strawberry' in favorite_fruits:
+    print('You really like strawberries')
+    
+if 'blueberry' in favorite_fruits:
+    print('You really like blueberries')
+    
+if 'apple' in favorite_fruits:
+    print('You really like apples')
+    
+#5.4
+age = 67
+
+if age < 10:
+    print('You are a kid')
+    
+elif age < 20:
+    print('You are a teenager')
+    
+else:
+    print('You are an adult')
+    if age > 65:
+        print('You are an elder')
---
+++ Test.py
@@ -0,0 +1,5 @@
+v1 = (1, 2, 3)
+print(len(v1))
+
+_1st_number = (1)
+print(_1st_number)
